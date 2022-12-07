e6b242df62704bd38a4e831c60353f87abe3e97e
Andrew Bowman <andrewtbowman@gmail.com>
2022-12-06 20:55:32 -0500

    Call array methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,43 @@
 friends = ["Pegah", "Tiffany", "Russell", "Naomi"]
 ages = [34, 33, 32, 31]
-heights = [1.1, 1.2, 1.3, 1.4]
+heights = [1.5, 1.6, 1.7, 1.8]
 cool = [true, true, true, true]
+
+puts "Here are my friends' names, ages, heights, and cool status:"
+p friends, ages, heights, cool
+
+# Calling push will add an element to the end of an array
+friends.push("Johnny")
+ages.push(30)
+heights.push(1.9)
+cool.push(true)
+
+puts "I pushed Johnny to the end of my friend group:"
+p friends, ages, heights, cool
+
+# Calling pop will remove the last element from the end of an array
+friends.pop
+ages.pop
+heights.pop
+cool.pop
+
+puts "Friendship with Johnny ended. I popped him back out of my group:"
+p friends, ages, heights, cool
+
+#Calling unshift will add an element to the start of an array
+friends.unshift("Sarah")
+ages.unshift(35)
+heights.unshift(1.4)
+cool.unshift(true)
+
+puts "I unshifted Sarah to the start of my friend group:"
+p friends, ages, heights, cool
+
+# Calling shift will remove the first element of an array
+friends.shift
+ages.shift
+heights.shift
+cool.shift
+
+puts "I shifted Sarah back out of my group:"
+p friends, ages, heights, cool
